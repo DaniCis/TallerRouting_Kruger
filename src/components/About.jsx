506e74fb5495fcc40ce9917c077d791f0ff59d24
DaniCis <danicisneros2@hotmail.com>
2022-11-25 19:12:03 -0500

    sobre mi terminado

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,8 +6,7 @@ const About = (props) =>{
             <img src={foto} alt="mi foto"/>
             <div className="contenedor-des">
                 <h4 className="about-title">{props.name}</h4>
-                <p className="about-des">Hi, I’m a System Engineer. I adapt to both individual and teamwork, always seeking to learn from my mistakes and the skills of those around me. 
-                My interests are focused on web development and Frontend programming along with interface design and user experience.</p>
+                <p className="about-des">Hi, I’m a System Engineer. I adapt to both individual and teamwork, always seeking to learn from my mistakes and the skills of those around me. My interests are focused on web development and Frontend programming along with interface design and user experience.</p>
             </div>
         </div>
     )
